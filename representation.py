8f7aec5301338732c563c6a7d12dbcb21c40a5cd
dreameverest2203 <amdekar.atharva@gmail.com>
2022-02-13 18:38:25 -0800

    absf

--- representation.py
+++ representation.py
@@ -51,90 +51,91 @@ def main(cfg: DictConfig) -> None:
     ) as handle:
         params, state = pickle.load(handle)
 
-    tqdm_data = tqdm.tqdm(train_data_loader)
-    num_comparisons, all_distorg, all_distaug = 0, 0, 0
-    for x, _ in tqdm_data:
-        # for j, (x, _) in enumerate(train_data_loader2):
-        x0 = x.permute(0, 2, 3, 1).numpy().reshape(data_shape)
-        aug0 = augmentation(x0).numpy()
-        augT = ode_sampler(
-            f, params, state, aug0, 25.0, 1e-3, chain_length=cfg.batch_size
-        )
-        xT = ode_sampler(f, params, state, x0, 25.0, 1e-3, chain_length=cfg.batch_size)
-        distorg = np.mean(np.linalg.norm(x0 - aug0, axis=(1, 2)))
-        distaug = np.mean(np.linalg.norm(xT - augT, axis=(1, 2)))
-        all_distorg += distorg
-        all_distaug += distaug
-        num_comparisons += 1
-
-        wandb.log(
-            data={
-                "Mean distance between x(0) and aug(0)": float(
-                    all_distorg / num_comparisons
-                )
-            }
-        )
-        wandb.log(
-            data={
-                "Mean distance between x(T) and aug(T)": float(
-                    all_distaug / num_comparisons
-                )
-            }
-        )
-        tqdm_data.set_description(
-            "Mean distance between x(0) and y(0): {:5f}".format(
-                all_distorg / num_comparisons
-            )
-        )
-
-        tqdm_data.set_description(
-            "Mean distance between x(T) and aug(T): {:5f}".format(
-                all_distaug / num_comparisons
-            )
-        )
-
-    # train_data_loader2 = DataLoader(
-    #     train_dataset, batch_size=2, shuffle=True, num_workers=2
-    # )
+    # tqdm_data = tqdm.tqdm(train_data_loader)
     # num_comparisons, all_distorg, all_distaug = 0, 0, 0
-    # tqdm_data = tqdm.tqdm(train_data_loader2)
     # for x, _ in tqdm_data:
     #     # for j, (x, _) in enumerate(train_data_loader2):
     #     x0 = x.permute(0, 2, 3, 1).numpy().reshape(data_shape)
     #     aug0 = augmentation(x0).numpy()
-    #     augT = ode_sampler(f, params, state, aug0, 25.0, 1e-3, chain_length=num_points)
-    #     xT = ode_sampler(f, params, state, x0, 25.0, 1e-3, chain_length=num_points)
-    #     distorg = np.linalg.norm(xT[0] - xT[1])
-    #     distaug = np.linalg.norm(augT[0] - augT[1])
+    #     augT = ode_sampler(
+    #         f, params, state, aug0, 25.0, 1e-3, chain_length=cfg.batch_size
+    #     )
+    #     xT = ode_sampler(f, params, state, x0, 25.0, 1e-3, chain_length=cfg.batch_size)
+    #     distorg = np.mean(np.linalg.norm(x0 - aug0, axis=(1, 2)))
+    #     distaug = np.mean(np.linalg.norm(xT - augT, axis=(1, 2)))
     #     all_distorg += distorg
     #     all_distaug += distaug
     #     num_comparisons += 1
 
     #     wandb.log(
     #         data={
-    #             "Mean distance between x(T) and y(T)": float(
+    #             "Mean distance between x(0) and aug(0)": float(
     #                 all_distorg / num_comparisons
     #             )
     #         }
     #     )
     #     wandb.log(
     #         data={
-    #             "Mean distance between xaug(0) and yaug(T)": float(
+    #             "Mean distance between x(T) and aug(T)": float(
     #                 all_distaug / num_comparisons
     #             )
     #         }
     #     )
     #     tqdm_data.set_description(
-    #         "Mean distance between x(T) and y(T): {:5f}".format(
+    #         "Mean distance between x(0) and y(0): {:5f}".format(
     #             all_distorg / num_comparisons
     #         )
     #     )
 
     #     tqdm_data.set_description(
-    #         "Mean distance between x_aug(T) and y_aug(T): {:5f}".format(
+    #         "Mean distance between x(T) and aug(T): {:5f}".format(
     #             all_distaug / num_comparisons
     #         )
     #     )
+
+    train_data_loader2 = DataLoader(
+        train_dataset, batch_size=2, shuffle=True, num_workers=2
+    )
+    num_comparisons, all_distorg, all_distaug = 0, 0, 0
+    tqdm_data = tqdm.tqdm(train_data_loader2)
+    for x, _ in tqdm_data:
+        x0 = x.permute(0, 2, 3, 1).numpy().reshape(data_shape)
+        aug0 = augmentation(x0).numpy()
+        augT = ode_sampler(
+            f, params, state, aug0, 25.0, 1e-3, chain_length=cfg.batch_size
+        )
+        xT = ode_sampler(f, params, state, x0, 25.0, 1e-3, chain_length=cfg.batch_size)
+        distorg = np.linalg.norm(xT[0] - xT[1])
+        distaug = np.linalg.norm(augT[0] - augT[1])
+        all_distorg += distorg
+        all_distaug += distaug
+        num_comparisons += 1
+
+        wandb.log(
+            data={
+                "Mean distance between x(T) and y(T)": float(
+                    all_distorg / num_comparisons
+                )
+            }
+        )
+        wandb.log(
+            data={
+                "Mean distance between xaug(0) and yaug(T)": float(
+                    all_distaug / num_comparisons
+                )
+            }
+        )
+        tqdm_data.set_description(
+            "Mean distance between x(T) and y(T): {:5f}".format(
+                all_distorg / num_comparisons
+            )
+        )
+
+        tqdm_data.set_description(
+            "Mean distance between x_aug(T) and y_aug(T): {:5f}".format(
+                all_distaug / num_comparisons
+            )
+        )
 
 
 if __name__ == "__main__":
